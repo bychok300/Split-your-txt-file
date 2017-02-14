43ab4d0c6d1acdb67efa7dbe9d2455f9090f96ce
Bychek Anton <bychok300@inbox.ru>
2017-02-14 17:17:23 +0300

    + deletind urls

--- src/parse_doc.py
+++ src/parse_doc.py
@@ -2,38 +2,52 @@
 import re
 import os
 
-#set path to your data
-input_path = 'data/za7.txt'         # here your should paste your path
-
-#set output path to your data
-output_path = 'output/za7spl.txt'   # here your should paste your output path
 
 #make new variable that list files in dir
 what_in_dir = os.listdir('data')
 
+# delete all urls from files
+for files in what_in_dir:
+    clean_list = []
+    with open('data/' + files, 'r', encoding="utf-8") as f_input:
+        for l in f_input:
+            if not l.startswith('http'):
+                clean_list.append(l)
+    
+    with open('data/' + files, 'w', encoding="utf-8") as f_out:
+        f_out.write(''.join(clean_list))
+
+
 #for files in dir open it and split on new doc
 #we should find pattern that we want recognize and split our data
 #my case is 2 new string simbols
-for i in what_in_dir:
-    print(i)
-    raw_file = open('data/' + i, mode='r')
-    articles = re.split(r'\n\s*\n', raw_file.read(), flags=re.M)
-
-
-
-#split our data to new file
-for i in range(len(articles)):
-    article = articles[i]
-    file_name = "output/{}{}.txt".format(article[:20], str(i))
-    file = open(file_name, mode='w', encoding='utf-8')
-    file.write(article)
-    file.close() 
-
-print(what_in_dir)
-
-#chek what we have in dir, if dir not empty delete all files from dir
-if not what_in_dir:
-    print('dir is empty')    
-else:
-    os.remove(input_path)
-    print('Your files has been deleted')
+  
+for files in what_in_dir:
+    print(files + '\n')
+    raw_file = open('data/' + files, mode='r', encoding='utf-8')
+    readed_file = raw_file.read()
+    articles = re.split(r'\n\s*\n', readed_file)
+  
+   
+   
+try:
+    #split our data to new file
+    for i in range(len(readed_file)):
+        article = articles[i]
+        file_name = "output/{}{}.txt".format(article[:50], str(i))
+        file = open(file_name, mode='w', encoding='utf-8')
+        file.write(article)
+        file.close() 
+        
+except(IndexError):
+    
+    print("End of list\n")
+          
+print("Numbers of files: ",len(os.listdir('output')))
+#     
+# #chek what we have in dir, if dir not empty delete all files from dir
+# if not what_in_dir:
+#     print('dir is empty')    
+# else:
+#     os.remove(input_path)
+#     print('Your files has been deleted')
